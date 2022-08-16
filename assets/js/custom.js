978bbd72ceb76cc1d74becf482f8c09e020f58b5
JEON-HYERIN <jeonhyelin19951118@gmail.com>
2022-08-16 23:50:06 +0900

    0816

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,5 +1,22 @@
-$('.btn-nav').click(function(e){
-  e.preventDefault();
-  $('#gnb').toggleClass('open');
-  $(this).toggleClass('close');
+$(function(){
+  // a태그 기본동작 방지
+  $(document).on('click', 'a[href="#"]', function (e) {
+    e.preventDefault();
+  });
+
+  // nav btn
+  $('.btn-nav').click(function(e){
+    e.preventDefault();
+    $('#gnb').toggleClass('open');
+    $(this).toggleClass('close');
+  });
+
+  // insight
+  const insightSwiper = new Swiper('.sc-insight .swiper', {
+    // autoplay: true,
+    // loop: true,
+    slidesPerView: true,
+    slidesPerGroupAuto: true,
+    grabCursor: true,
+  });
 });
