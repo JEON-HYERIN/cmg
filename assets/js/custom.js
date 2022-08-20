58fde33cb696007bae03ba5b480dcb158d9a5284
JEON-HYERIN <jeonhyelin19951118@gmail.com>
2022-08-20 23:46:18 +0900

    0820

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -19,11 +19,48 @@ $(function(){
     })
   });
 
-  // nav btn
+  // btn nav
   $('.btn-nav').click(function(e){
     e.preventDefault();
     $('#gnb').toggleClass('open');
     $(this).toggleClass('close');
+
+    if($('#gnb').hasClass('open')) {
+      $('#gnb .gnb-item').each(function(index, element) {
+        const anim = gsap.fromTo(element, {
+          opacity:0,
+          x: '1rem',
+          visibility: 'hidden',
+        }, {
+          x: 0,
+          opacity:1,
+          visibility: 'inherit',
+          delay: (index + 1) * .2,
+          duration: 0.25
+        });
+        ScrollTrigger.create({
+          trigger: '#gnb.open',
+          start:'top center',
+          animation: anim,
+          toggleActions: 'play none none none',
+          // play pause resume reset
+          once: true,
+        });
+      });
+      const tlNav = gsap.timeline({
+        defaults: {
+          delay: 1.4
+        },
+        scrollTrigger:{
+          trigger: '#gnb.open',
+          start:'top center',
+          // markers:true,
+        },
+      })
+      tlNav.addLabel('t1')
+      .fromTo('#gnb .signup-area',{opacity:0, x:'1rem'},{opacity:1, x:0, duration: 0.25},'t1')
+      .fromTo('#gnb .connect-area',{opacity:0, x:'1rem'},{opacity:1, x:0, duration: 0.25},'t1+=.2')
+    }
   });
 
   // visual
@@ -77,6 +114,26 @@ $(function(){
       });
     }
   });
+  
+  $('.sc-visual .title .row span').each(function(index, element) {
+    const anim = gsap.fromTo(element, {
+      opacity:0,
+      y: 70,
+      visibility: 'hidden',
+    }, {
+      y: 0,
+      opacity:1,
+      visibility: 'inherit',
+      delay: (index + 1) * .2,
+    });
+    ScrollTrigger.create({
+      trigger: element,
+      animation: anim,
+      toggleActions: 'play none none none',
+      // play pause resume reset
+      once: true,
+    });
+  });
 
   // intro
   toggleClass();
@@ -118,7 +175,135 @@ $(function(){
     }
   });
 
+  const tlIntro = gsap.timeline({
+    scrollTrigger:{
+      trigger:".sc-intro .group-title",
+      start:'top center',
+      // markers:true,
+    },
+  })
+  tlIntro.addLabel('t1')
+  .fromTo('.sc-intro .title-area .sc-title',{opacity:0, x:70},{opacity:1, x:0},'t1')
+  .fromTo('.sc-intro .title-area .desc',{opacity:0, x:70},{opacity:1, x:0},'t1+=.2')
+
+  // whatwedo
+  ScrollTrigger.matchMedia({
+    "(min-width: 1024px)": function(){
+      const tlWhatwedo = gsap.timeline({
+        scrollTrigger:{
+          trigger:'.sc-whatwedo .group-title',
+          start:'top center',
+          // markers:true,
+          toggleActions: 'play none none none',
+          // play pause resume reset
+          once: true,
+        }
+      })
+      tlWhatwedo.addLabel('t1')
+      .fromTo('.sc-whatwedo .sc-title',{opacity:0, y:70, visibility: 'hidden'},{opacity:1, y:0, visibility: 'inherit'},'t1')
+      .fromTo('.sc-whatwedo .sc-sub-title .txt1',{opacity:0, y:70, visibility: 'hidden'},{opacity:1, y:0, visibility: 'inherit'},'t1+=.2')
+      .fromTo('.sc-whatwedo .sc-sub-title .txt2',{opacity:0, y:70, visibility: 'hidden'},{opacity:1, y:0, visibility: 'inherit'},'t1+=.4')
+      .fromTo('.sc-whatwedo .sc-sub-title .txt3',{opacity:0, y:70, visibility: 'hidden'},{opacity:1, y:0, visibility: 'inherit'},'t1+=.6')
+      .fromTo('.sc-whatwedo .title-area .desc',{opacity:0, y:70, visibility: 'hidden'},{opacity:1, y:0, visibility: 'inherit'},'t1+=.8')
+    
+      $('.sc-whatwedo .whatwedo-item').each(function(index, element) {
+        const anim = gsap.fromTo(element, {
+          opacity:0,
+          y: 70,
+          visibility: 'hidden',
+        }, {
+          y: 0,
+          opacity:1,
+          visibility: 'inherit',
+          delay: (index + 1) * .2,
+        });
+        ScrollTrigger.create({
+          trigger: '.sc-whatwedo .group-title',
+          start:'top center',
+          animation: anim,
+          toggleActions: 'play none none none',
+          // play pause resume reset
+          once: true,
+        });
+      });
+    },
+    "(max-width: 1023px)": function(){
+      const tlWhatwedo = gsap.timeline({
+        scrollTrigger:{
+          trigger:'.sc-whatwedo .group-title',
+          start:'top center',
+          // markers:true,
+          toggleActions: 'play none none none',
+          // play pause resume reset
+          once: true,
+        }
+      })
+      tlWhatwedo.addLabel('t1')
+      .fromTo('.sc-whatwedo .sc-title',{opacity:0, y:70, visibility: 'hidden'},{opacity:1, y:0, visibility: 'inherit'},'t1')
+      .fromTo('.sc-whatwedo .sc-sub-title .txt1',{opacity:0, y:70, visibility: 'hidden'},{opacity:1, y:0, visibility: 'inherit'},'t1+=.2')
+      .fromTo('.sc-whatwedo .sc-sub-title .txt2',{opacity:0, y:70, visibility: 'hidden'},{opacity:1, y:0, visibility: 'inherit'},'t1+=.4')
+      .fromTo('.sc-whatwedo .sc-sub-title .txt3',{opacity:0, y:70, visibility: 'hidden'},{opacity:1, y:0, visibility: 'inherit'},'t1+=.6')
+      .fromTo('.sc-whatwedo .title-area .desc',{opacity:0, y:70, visibility: 'hidden'},{opacity:1, y:0, visibility: 'inherit'},'t1+=.8')
+    
+      $('.sc-whatwedo .whatwedo-item').each(function(index, element) {
+        const anim = gsap.fromTo(element, {
+          opacity:0,
+          y: 70,
+          visibility: 'hidden',
+        }, {
+          y: 0,
+          opacity:1,
+          visibility: 'inherit',
+          delay: (index + 1) * .2,
+        });
+        ScrollTrigger.create({
+          trigger: '.whatwedo-list',
+          start:'top 65%',
+          animation: anim,
+          toggleActions: 'play none none none',
+          // play pause resume reset
+          once: true,
+        });
+      });
+    }
+  });
+
   // client
+  const tlClient = gsap.timeline({
+    scrollTrigger:{
+      trigger:'.sc-client',
+      start:'top center',
+      // markers:true,
+      toggleActions: 'play none none none',
+      // play pause resume reset
+      once: true,
+    }
+  })
+  tlClient.addLabel('t1')
+  .fromTo('.sc-client .sc-title',{opacity:0, y:70, visibility: 'hidden'},{opacity:1, y:0, visibility: 'inherit'},'t1')
+  .fromTo('.sc-client .sc-sub-title',{opacity:0, y:70, visibility: 'hidden'},{opacity:1, y:0, visibility: 'inherit'},'t1+=.2')
+
+  $('.sc-client .client-item').each(function(index, element) {
+    const anim = gsap.fromTo(element, {
+      opacity:0,
+      y: 70,
+      visibility: 'hidden',
+    }, {
+      y: 0,
+      opacity:1,
+      visibility: 'inherit',
+      // delay: (index + 1) * .2,
+    });
+    ScrollTrigger.create({
+      trigger: element,
+      start:'top center',
+      animation: anim,
+      toggleActions: 'play none none none',
+      // play pause resume reset
+      once: true,
+    });
+  });
+
   const clientSwiper = new Swiper('.sc-client .client-list-wrap.swiper', {
     // autoplay: true,
     // loop: true,
@@ -152,8 +337,10 @@ $(function(){
   })
 
   op.addLabel('m1')
-  .fromTo('.sc-study .bg2',{opacity:0},{opacity:1},'m1+=1')
-  .fromTo('.sc-study .bg3',{opacity:0},{opacity:1},'m1+=2')
+  // .fromTo('.sc-study .bg2',{opacity:0},{opacity:1},'m1+=1')
+  // .fromTo('.sc-study .bg3',{opacity:0},{opacity:1},'m1+=2')
+  .fromTo('.sc-study .bg2',{display: 'none', opacity: 0},{display: 'block', opacity: 1},'m1+=1')
+  .fromTo('.sc-study .bg3',{display: 'none', opacity: 0},{display: 'block', opacity: 1},'m1+=2')
 
   const studySwiper = new Swiper('.sc-study .study-list-wrap.swiper', {
     direction: 'vertical',
@@ -164,20 +351,67 @@ $(function(){
     },
   });
 
-  // insight
-  gsap.fromTo('.sc-insight .insight-list', {
-    opacity: 0,
-    y: 100,
-  }, {
-    scrollTrigger: {
-      trigger: '.sc-insight',
-      start: 'top top', 
-      end: 'bottom top',
-      // markers: true,
-    },
-    opacity: 1,
-    y: 0
+  ScrollTrigger.saveStyles('.sc-study .sc-title');
+  ScrollTrigger.saveStyles('.sc-study .badge');
+  ScrollTrigger.matchMedia({
+    "(max-width: 1023px)": function(){    
+      $('.sc-study .badge').each(function(index, element) {
+        const anim = gsap.fromTo(element, {
+          opacity:0,
+          x: '1rem',
+          visibility: 'hidden',
+        }, {
+          opacity:1,
+          x: 0,
+          visibility: 'inherit',
+          // delay: (index + 1) * .2,
+        });
+        ScrollTrigger.create({
+          trigger: element,
+          start:'top 75%',
+          animation: anim,
+          toggleActions: 'play none none none',
+          // play pause resume reset
+          once: true,
+          // markers: true,
+        });
+      });
+      const animStudy = gsap.fromTo('.sc-study .sc-title', {
+        opacity:0,
+        x: '1rem',
+        visibility: 'hidden',
+      }, {
+        opacity:1,
+        x: 0,
+        visibility: 'inherit',
+      });
+      ScrollTrigger.create({
+        trigger: '.sc-study',
+        start:'top 60%',
+        animation: animStudy,
+        toggleActions: 'play none none none',
+        // play pause resume reset
+        once: true,
+        // markers: true
+      });
+    }
   });
+
+  // insight
+  const tlInsight = gsap.timeline({
+    scrollTrigger:{
+      trigger:'.sc-insight',
+      start:'top center',
+      // markers:true,
+      toggleActions: 'play none none none',
+      // play pause resume reset
+      once: true,
+    }
+  })
+  tlInsight.addLabel('t1')
+  .fromTo('.sc-insight .sc-sub-title .txt1',{opacity:0, y:70, visibility: 'hidden'},{opacity:1, y:0, visibility: 'inherit'},'t1')
+  .fromTo('.sc-insight .sc-sub-title .txt2',{opacity:0, y:70, visibility: 'hidden'},{opacity:1, y:0, visibility: 'inherit'},'t1+=.2')
+  .fromTo('.sc-insight .insight-list',{opacity:0, y:100, visibility: 'hidden'},{opacity:1, y:0, visibility: 'inherit'},'t1+=.4')
 
   const insightSwiper = new Swiper('.sc-insight .insight-list-wrap.swiper', {
     slidesPerView: 1.4,
@@ -194,7 +428,27 @@ $(function(){
       }
     }
   });
-  
+
+  // footer
+  const animFoot = gsap.fromTo('.footer-title', {
+    opacity:0,
+    yPercent: 100,
+    visibility: 'hidden',
+  }, {
+    yPercent: 0,
+    opacity:1,
+    visibility: 'inherit',
+  });
+  ScrollTrigger.create({
+    trigger: '#footer',
+    start:'top 60%',
+    animation: animFoot,
+    toggleActions: 'play none none none',
+    // play pause resume reset
+    once: true,
+    // markers: true
+  });
+
   // footer copyright 
   const year = new Date().getFullYear();
   $('.copyright .year').text(year);
