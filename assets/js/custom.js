81974370f37993ae4b0202dd4b53830e0e534dd6
JEON-HYERIN <jeonhyelin19951118@gmail.com>
2022-08-30 00:28:11 +0900

    0830

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -74,7 +74,7 @@ $(function(){
   $('.btn-nav').click(function(e){
     e.preventDefault();
     $('#gnb').toggleClass('open');
-    $(this).toggleClass('close');
+    $(this).toggleClass('on');
 
     if($('#gnb').hasClass('open')) {
       enable();
